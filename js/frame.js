dd78a09369948cd6a76caa81c31268d77ed51258
wacchi2940 <wacchi.2940@gmail.com>
2026-06-01 21:45:39 +0900

    Update frame.js

--- js/frame.js
+++ js/frame.js
@@ -1701,7 +1701,7 @@ function applyStickyColumns(state) {
             if (!cell) return;
 
             cell.classList.add('sticky-column');
-            cell.style.backgroundColor = "red"; // テスト
+            cell.style.backgroundColor = "blue"; // テスト
             cell.style.left = `${left}px`;
 
             left += widths[idx];
